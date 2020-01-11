52e1fd3912cc5086e710aa5f7029cb20d87ba653
Kamaldwip Choudhury (V) <kamaldwip_choudhury@apple.com>
2020-01-11 03:21:25 -0600

    Add JPA entities

---
+++ src/test/java/com/kamal/springframework/guru/spring5demowebapp/Spring5demowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamal.springframework.guru.spring5demowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5demowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
